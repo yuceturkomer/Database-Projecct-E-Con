769bc9779e98d62d3117391345cb8c6dc10515ee
Ömer Faruk Yücetürk <oyuceturk@gtu.edu.tr>
2017-06-04 16:25:02 +0300

    Kappa
    123

--- Gtu E-Con/About.aspx.cs
+++ Gtu E-Con/About.aspx.cs
@@ -4,12 +4,40 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Oracle.ManagedDataAccess.Client;
+using Oracle.ManagedDataAccess.Types;
+
 
 namespace Gtu_E_Con
 {
     public partial class About : Page
     {
+        OracleConnection con;
+
         protected void Page_Load(object sender, EventArgs e)
+        {
+            String conStr = "USER ID=lelo;Password=123;Data Source=localhost:1521/xe";
+            con = new OracleConnection();
+            con.ConnectionString = conStr;
+            con.Open();
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+            OracleCommand Query = con.CreateCommand();
+            string mail = mailbox.Text;
+            string pw = pwbox.Text;
+            namebox.Text = mail + "  " + pw;
+            
+
+            Query.CommandText = "INSERT INTO MUZ VALUES('"+mail+"',"+pw+")";
+            Query.ExecuteReader();
+
+        }
+
+        protected void FormView1_PageIndexChanging(object sender, FormViewPageEventArgs e)
         {
 
         }
